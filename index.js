6ab514bf8c0a87587fa3324275e612016c86e25c
Brooke Fisher <bfisher@hubspot.com>
2022-09-27 16:09:50 -0400

    Built calendar and instantiated a new calendar event for each custom object

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 
 
 const app = express();
+
+require('dotenv').config();
+// console.log(process.env);
  
 app.set('view engine', 'pug');
 
@@ -13,21 +16,24 @@ app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', async (req, res) => {
-    res.render('index')
-    // // Get all rental equipment data
-    // const apiCall = 'https://api.hubapi.com/crm/v3/objects/{objectType}/?limit=10&archived=false';
-    // const headers = {
-    //     Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-    //     'Content-Type': 'application/json'
-    // }
-    // try {
-    //     const resp = await axios.get(apiCall, { headers });
-    //     const data = resp.data.results;
-        res.render('index', { title: 'AV Rental Equipment | HubSpot APIs', data });      
-    // } 
-    // catch (error) {
-    //     console.error(error);
-    // }
+    res.render('index', { title: 'AV Rental Equipment | HubSpot APIs' });
+});
+
+app.get('/get-data', async (req, res) => {
+   // Get all rental equipment data
+    const apiCall = 'https://api.hubapi.com/crm/v3/objects/2-8923644/?limit=25&archived=false&properties=name,start_date,end_date';
+    const headers = {
+        Authorization: `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(apiCall, { headers });
+        const data = resp.data.results;
+        res.json(data);      
+    } 
+    catch (error) {
+        console.error(error);
+    } 
 });
 
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
--- public/calendar.js
+++ public/calendar.js
@@ -3,28 +3,46 @@ import Calendar from '@toast-ui/calendar';
 import '@toast-ui/calendar/dist/toastui-calendar.min.css';
 import './css/style.css'
 
-const calendar = new Calendar('#calendar', {
-    defaultView: 'week',
-    template: {
-      time(event) {
-        const { start, end, title } = event;
-  
-        return `<span style="color: white;">${formatTime(start)}~${formatTime(end)} ${title}</span>`;
-      },
-      allday(event) {
-        return `<span style="color: gray;">${event.title}</span>`;
-      },
-    },
-    calendars: [
-      {
-        id: 'cal1',
-        name: 'Personal',
-        backgroundColor: '#03bd9e',
-      },
-      {
-        id: 'cal2',
-        name: 'Work',
-        backgroundColor: '#00a9ff',
-      },
-    ],
-  });
+// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+// dotenv.config()
+// import express from 'express'
+
+async function createCalendar() {
+  try {
+    const resp = await fetch(`http://localhost:3000/get-data`);
+    const data = await resp.json();
+    console.log(data);  
+    
+    // Create Calendar instance
+    const calendar = new Calendar('#calendar', {
+      defaultView: 'month',
+      calendars: [
+        {
+          id: 'cal1',
+          name: 'AV Rental',
+          backgroundColor: '#03bd9e',
+        }
+      ],
+    });
+
+    calendar.createEvents(
+      // Create indiviaul events from /get-data
+      data.map((rental) => {
+        return {
+          id: rental.id,
+          calendarId: 'cal1',
+          title: rental.properties.name,
+          category: 'allday',
+          dueDateClass: '',
+          start: rental.properties.start_date,
+          end: rental.properties.end_date,
+        }
+      })
+    );
+  } 
+  catch (error) {
+      console.error(error);
+  }   
+}
+
+createCalendar();
