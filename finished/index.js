1b7028fb66034eb85f2675ed10f87f12dfa5ae1a
Brooke Fisher <bfisher@hubspot.com>
2022-10-03 14:57:44 -0400

    Add comments to finished files, and include index.pug

--- finished/index.js
+++ finished/index.js
@@ -1,27 +1,60 @@
+/**
+ * * Call the consts you need to run Express, axios, and body-parser
+ */ 
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 
+//* Instantiate Express
 const app = express();
 
+//* Express app.use for JSON encoding
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//* Use dotenv inside of a node app
 require('dotenv').config();
-// console.log(process.env);
- 
+
+//* Use pug as the view engine
 app.set('view engine', 'pug');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//* Use this to serve up static files from public directory
 app.use(express.static(__dirname + '/public'));
 
-
+/**  
+ * * First, we'll create a route at the root (/) where our web app will appear.
+ */
 app.get('/', async (req, res) => {
     res.render('index', { title: 'AV Rental Equipment | HubSpot APIs' });
 });
 
-app.post('/', async (req, res) => {
+/**
+ * * Next, we'll get the existing data from our custom object to show on our calendar. The data we're calling is from the CSV file we imported.
+ */
+ app.get('/get-data', async (req, res) => {
+    // Get all rental equipment data
+     const apiCall = 'https://api.hubapi.com/crm/v3/objects/2-8923644/?limit=25&archived=false&properties=name,start_date,end_date';
+     const headers = {
+         Authorization: `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
+         'Content-Type': 'application/json'
+     }
+     try {
+         const resp = await axios.get(apiCall, { headers });
+         const data = resp.data.results;
+         res.json(data);      
+     } 
+     catch (error) {
+         console.error(error);
+     } 
+ });
+
+/**
+ * * Now that we've built our form, it's time to send that data to our HubSpot account upon submit.
+ */
+ app.post('/', async (req, res) => {
     const newEquipmentRental = {
         properties: {
             "name": req.body.chooseEquipment,
@@ -44,21 +77,5 @@ app.post('/', async (req, res) => {
     }
 });
 
-app.get('/get-data', async (req, res) => {
-   // Get all rental equipment data
-    const apiCall = 'https://api.hubapi.com/crm/v3/objects/2-8923644/?limit=25&archived=false&properties=name,start_date,end_date';
-    const headers = {
-        Authorization: `Bearer ${process.env.PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(apiCall, { headers });
-        const data = resp.data.results;
-        res.json(data);      
-    } 
-    catch (error) {
-        console.error(error);
-    } 
-});
-
+//* This is how we'll listen on port 3000 when we call nodemon.
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
--- finished/public/calendar.js
+++ finished/public/calendar.js
@@ -1,55 +1,69 @@
-/* ES6 module in Node.js environment */
-import Calendar from '@toast-ui/calendar';
-import '@toast-ui/calendar/dist/toastui-calendar.min.css';
-import './css/style.css'
-
-// import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-// dotenv.config()
-// import express from 'express'
-
-async function createCalendar() {
-  try {
-    const resp = await fetch(`http://localhost:3000/get-data`);
-    const data = await resp.json();
-    const dataMap = data.map((rental) => {
-      return {
-        id: rental.id,
-        calendarId: rental.properties.name,
-        title: rental.properties.name,
-        category: 'allday',
-        dueDateClass: '',
-        start: rental.properties.start_date,
-        end: rental.properties.end_date,
-      }
-    });
-    console.log(dataMap);
-
-    const calendars = [];
-    dataMap.forEach(rental => {
-      const avCalendars = 
-        {
-          id: rental.calendarId,
-          name: rental.title,
-          backgroundColor: `#${Math.floor(Math.random()*16777215).toString(16)}`,
-        }
-      calendars.push(avCalendars);     
-    });
-
-    console.log(calendars);
-    
-    // Create Calendar instance
-    const calendar = new Calendar('#calendar', {
-      defaultView: 'month',
-      calendars: calendars,
-    });
-
-    calendar.createEvents(dataMap);
-    const month = calendar.getDate();
-    document.getElementById('month-heading').innerHTML = `${new Date(month).toLocaleString('en-us',{month:'long', year:'numeric'})}`;
-  } 
-  catch (error) {
-      console.error(error);
-  }   
-}
-
-createCalendar();
+/**
+ * * ES6 module in Node.js environment
+ * * These import the JS and CSS for the front-end.
+ */
+ import Calendar from '@toast-ui/calendar';
+ import '@toast-ui/calendar/dist/toastui-calendar.min.css';
+ import './css/style.css'
+ 
+ /**
+  * * Here, we're creating a function called createCalendar(). This function: 
+  * * 1.) calls the data GET data from our /get-data route.
+  * * 2.) Creates an array of objects to turn the data we receive from HubSpot into one useable by the Calendar library.
+  * * 3.) Loops over the array to create another array of objects to instantiate different colored calendars for each custom object data. 
+  * * 4.) Instantiates the calendar.
+  */
+ async function createCalendar() {
+   //* Wrap everything in a try/catch for error handling.
+   try {
+     const resp = await fetch(`http://localhost:3000/get-data`);
+     const data = await resp.json();
+     const dataMap = data.map((rental) => {
+       return {
+         id: rental.id,
+         calendarId: rental.properties.name,
+         title: rental.properties.name,
+         category: 'allday',
+         dueDateClass: '',
+         start: rental.properties.start_date,
+         end: rental.properties.end_date,
+       }
+     });
+ 
+     //* Testing to make sure your array of objects looks correct
+     console.log(dataMap);
+ 
+     const calendars = [];
+     dataMap.forEach(rental => {
+       const avCalendars = 
+         {
+           id: rental.calendarId,
+           name: rental.title,
+           backgroundColor: `#${Math.floor(Math.random()*16777215).toString(16)}`,
+         }
+       calendars.push(avCalendars);     
+     });
+ 
+     //* Testing to make sure your array of objects looks correct
+     console.log(calendars);
+     
+     //* Create Calendar instance
+     const calendar = new Calendar('#calendar', {
+       defaultView: 'month',
+       calendars: calendars,
+     });
+ 
+     //* Add the individual events to calendar
+     calendar.createEvents(dataMap);
+ 
+     //* Heading above calendar to show month and year
+     const month = calendar.getDate();
+     document.getElementById('month-heading').innerHTML = `${new Date(month).toLocaleString('en-us',{month:'long', year:'numeric'})}`;
+   } 
+   catch (error) {
+       console.error(error);
+   }   
+ }
+ 
+ //* Always call your function.
+ createCalendar();
