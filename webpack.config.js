d1583159a8cdd0b528a81efe35e9dfd58f87dddc
Ben Brophy <bbrophy@hubspot.com>
2022-10-06 17:45:24 -0400

    Added build scripts; added basic get route; updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,36 @@
 const path = require('path');
 const webpack = require('webpack');
-const TerserPlugin = require("terser-webpack-plugin");
 const autoprefixer = require('autoprefixer');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const javascript = {
-  test: /\.(js)$/, // see how we match anything that ends in `.js`? Cool
+  test: /\.(js)$/, 
   use: [{
     loader: 'babel-loader',
-    options: { presets: ['@babel/preset-env'] } // this is one way of passing options
+    options: { presets: ['@babel/preset-env'] }
   }],
 };
 
-const postcss = {
-    loader: 'postcss-loader',
-    options: {
-      plugins() { return [autoprefixer({ browsers: 'last 3 versions' })]; }
-    }
-  };
-
 const config = {
+  mode: 'development',
   entry: {
     calendar: './public/calendar.js'
   },
   devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'public', 'dist'),
-    filename: '[name].min.js'
+    filename: '[name].bundled.js'
   },
   module: {
     rules: [javascript,  {
         test: /\.(css)$/,
-        use: ['style-loader','css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
     }]
   },
-  optimization: {
-    minimize: true,
-    minimizer: [new TerserPlugin()],
-  },
+  plugins: [new MiniCssExtractPlugin({
+    filename: '[name].css',
+  })],
   
 };
-
-process.noDeprecation = true;
 
 module.exports = config;
